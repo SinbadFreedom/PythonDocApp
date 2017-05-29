5ff71408e1b4373c0db7b0cd2f13a3cecadd3c60
SinbadFreedom <151229152@qq.com>
2017-05-29 12:20:15 +0800

    remove ViewPagerAndroid, add hardwareBackPress listener to control WebView goback.

--- index.android.js
+++ index.android.js
@@ -1,145 +1,49 @@
-'use strict';
-
 import React, {Component} from 'react';
 import {
     AppRegistry,
-    StatusBar,
     WebView,
+    StatusBar,
     StyleSheet,
     View,
-    Image,
-    ViewPagerAndroid,
     BackHandler
 } from 'react-native';
 
-import {ViewPagerScrollState} from 'ViewPagerAndroid';
+const styles = StyleSheet.create({
+                                     container: {
+                                         flex: 1,
+                                         backgroundColor: '#1FB9FF',
+                                         borderWidth: 0
+                                     },
+                                 });
 
-var BGCOLOR = ['#fdc08e', '#fff6b9', '#99d1b7', '#dde5fe', '#f79273'];
-var IMAGE_URIS = [
-    'http://apod.nasa.gov/apod/image/1410/20141008tleBaldridge001h990.jpg',
-    'http://apod.nasa.gov/apod/image/1409/volcanicpillar_vetter_960.jpg',
-    'http://apod.nasa.gov/apod/image/1409/m27_snyder_960.jpg',
-    'http://apod.nasa.gov/apod/image/1409/PupAmulti_rot0.jpg',
-    'http://apod.nasa.gov/apod/image/1510/lunareclipse_27Sep_beletskycrop4.jpg',
-];
+const WEB_VIEW_REF = "webView";
 
-var PythonDocApp = React.createClass({
-                                         statics: {
-                                             title: '<ViewPagerAndroid>',
-                                             description: 'Container that allows to flip left and right between child views.'
-                                         },
-                                         getInitialState: function () {
-                                             return {
-                                                 page: 0,
-                                                 animationsAreEnabled: true,
-                                                 scrollEnabled: true,
-                                                 progress: {
-                                                     position: 0,
-                                                     offset: 0,
-                                                 },
-                                             };
-                                         },
+class PythonDocApp extends Component {
 
-                                         onPageSelected: function (e) {
-                                             this.setState({page: e.nativeEvent.position});
-                                         },
+    componentDidMount() {
+        BackHandler.addEventListener('hardwareBackPress', this.backHandler);
+    }
 
-                                         onPageScroll: function (e) {
-                                             this.setState({progress: e.nativeEvent});
-                                         },
+    componentWillUnmount() {
+        BackHandler.removeEventListener('hardwareBackPress', this.backHandler);
+    }
 
-                                         onPageScrollStateChanged: function (state: ViewPagerScrollState) {
-                                             this.setState({scrollState: state});
-                                         },
+    backHandler = () => {
+        this.refs[WEB_VIEW_REF].goBack();
+        return true;
+    };
 
-                                         move: function (delta) {
-                                             var page = this.state.page + delta;
-                                             this.go(page);
-                                         },
-
-                                         go: function (page) {
-                                             if (this.state.animationsAreEnabled) {
-                                                 this.viewPager.setPage(page);
-                                             } else {
-                                                 this.viewPager.setPageWithoutAnimation(page);
-                                             }
-
-                                             this.setState({page});
-                                         },
-
-                                         render: function () {
-                                             var pages = [];
-                                             for (var i = 0; i < IMAGE_URIS.length; i++) {
-                                                 var pageStyle = {
-                                                     backgroundColor: BGCOLOR[i % BGCOLOR.length],
-                                                     alignItems: 'center',
-                                                     padding: 20,
-                                                 };
-                                                 pages.push(
-                                                     <View key={i} style={pageStyle} collapsable={false}>
-                                                         <Image
-                                                             style={styles.image}
-                                                             source={{uri: IMAGE_URIS[i % BGCOLOR.length]}}
-                                                         />
-                                                     </View>
-                                                 );
-                                             }
-
-                                             pages.push(
-                                                 <View style={styles.container}>
-                                                     <StatusBar hidden={true}></StatusBar>
-                                                     <WebView source={ {uri : 'http://139.59.45.254/html/'}}
-                                                              onNavigationStateChange={(event) =>{console.log("onNavigationStateChange: " + event)}}
-                                                              onMessage={(event) => {console.log(" onMessage " + event.nativeEvent)}}>
-                                                     </WebView>
-                                                     {/*<WebView source={require("./html/index.html")}/>*/}
-                                                 </View>
-                                             );
-
-                                             return (
-                                                 <View style={styles.container}>
-                                                     <ViewPagerAndroid
-                                                         style={styles.viewPager}
-                                                         initialPage={0}
-                                                         scrollEnabled={this.state.scrollEnabled}
-                                                         onPageScroll={this.onPageScroll}
-                                                         onPageSelected={this.onPageSelected}
-                                                         onPageScrollStateChanged={this.onPageScrollStateChanged}
-                                                         pageMargin={10}
-                                                         ref={viewPager => { this.viewPager = viewPager; }}>
-                                                         {pages}
-                                                     </ViewPagerAndroid>
-                                                 </View>
-                                             );
-                                         },
-                                     });
-
-var styles = StyleSheet.create({
-                                   container: {
-                                       flex: 1,
-                                       backgroundColor: 'white',
-                                   },
-                                   image: {
-                                       width: 300,
-                                       height: 200,
-                                       padding: 20,
-                                   },
-                                   viewPager: {
-                                       flex: 1,
-                                   },
-                               });
-
-BackHandler.addEventListener('hardwareBackPress', function() {
-    console.log("-----------------");
-    // this.onMainScreen and this.goBack are just examples, you need to use your own implementation here
-    // Typically you would use the navigator here to go to the last state.
-    // return true;
-    // if (!this.onMainScreen()) {
-    //     this.goBack();
-    //     return true;
-    // }
-    // return false;
-    return true;
-});
+    render() {
+        return (
+            <View style={styles.container}>
+                <StatusBar hidden={true}></StatusBar>
+                <WebView
+                    ref={WEB_VIEW_REF}
+                    source={ {uri : 'http://139.59.45.254/html/'}}
+                />
+            </View>
+        );
+    }
+}
 
 AppRegistry.registerComponent('PythonDocApp', () => PythonDocApp);
